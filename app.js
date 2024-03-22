5d3321bbd47d01f4a893620c1b0f1a458fa331cd
Adhish1612Acharya <adhisha0505@gmail.com>
2024-03-22 22:13:45 +0530

    added env variable to app

--- app.js
+++ app.js
@@ -49,7 +49,7 @@ app.engine("ejs",ejsMate);
 const store = MongoStore.create({
     mongoUrl: db_URL,
     crypto: {
-      secret: "mysecretcode",
+      secret: process.env.SECRET,
     },
     touchAfter: 24*3600,
   });
@@ -60,7 +60,7 @@ store.on("error",(err)=>{
 
 const sessionOptions={
     store, //store:store,
-    secret:"mysecretcode",
+    secret:process.env.SECRET,
     resave:false,
     saveUninitialized:true,
     cookie:{
